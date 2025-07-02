958e6dd96c7a0a2b94bae531d209ac85ab05dbc8
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2025-07-02 15:36:58 +0100

    chore: change names of S3 stack

--- chatbot_stacks/S3Stack.py
+++ chatbot_stacks/S3Stack.py
@@ -3,16 +3,20 @@ from aws_cdk import (
     Stack,
     aws_s3 as s3,
     aws_s3_deployment as s3deploy,
+    RemovalPolicy,
 )
 from constructs import Construct
 
-class Chatbot1Stack(Stack):
+class S3Stack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        bucket = s3.Bucket(self, f"chatbot-bucket-{self.account}", bucket_name=f"chatbot-bucket-{self.account}")
+        bucket = s3.Bucket(self, f"chatbot-bucket-{self.account}",
+                            bucket_name=f"chatbot-bucket-{self.account}",
+                            removal_policy=RemovalPolicy.DESTROY,
+                            auto_delete_objects=True)
         
         s3deploy.BucketDeployment(self, "deploy-chatbot-bucket",
             sources=[s3deploy.Source.asset("./sagemaker_documentation")],
--- tests/unit/test_chatbot_1_stack.py
+++ tests/unit/test_chatbot_1_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from chatbot_1.chatbot_1_stack import Chatbot1Stack
+from chatbot_stacks.S3Stack import Chatbot1Stack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in chatbot_1/chatbot_1_stack.py
