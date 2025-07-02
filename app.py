83ef5e0e885dd3da1017275df56e1eec090c8d34
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2025-07-02 16:08:58 +0100

    refactor: change s3 stack name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import aws_cdk as cdk
 
 from config import EnvSettings
-from chatbot_stacks.S3Stack import S3Stack
+from chatbot_stacks.s3Stack import S3Stack
 from chatbot_stacks.kb_role_stack import KbRoleStack
 from chatbot_stacks.oss_infra_stack import OpenSearchServerlessStack
 
