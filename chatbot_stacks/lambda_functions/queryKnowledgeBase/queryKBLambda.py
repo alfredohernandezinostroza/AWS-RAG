33732af6f577bf031e50f74f1c030487e03489a4
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2025-07-02 16:41:12 +0100

    fix: lambda function for querying

--- chatbot_stacks/lambda_functions/queryKnowledgeBase/queryKBLambda.py
+++ chatbot_stacks/lambda_functions/queryKnowledgeBase/queryKBLambda.py
@@ -8,22 +8,20 @@ bedrock_agent_runtime_client = client("bedrock-agent-runtime", region_name=os.en
 def lambda_handler(event, context):
     question = json.loads(event["body"])["question"]
 
-    input_data = {
-        "input": {
-            "text": question
-        },
-        "retrieveAndGenerateConfiguration": {
+    response = bedrock_agent_runtime_client.retrieve_and_generate(
+        input={
+            'text': question
+        },  
+        retrieveAndGenerateConfiguration = {
             "type": "KNOWLEDGE_BASE",
             "knowledgeBaseConfiguration": {
                 "knowledgeBaseId": os.environ["KNOWLEDGE_BASE_ID"],
-                "modelArn": f"arn:aws:bedrock:{os.environ['AWS_REGION']}::foundation-model/anthropic.claude-v2"
+                "modelArn": f"arn:aws:bedrock:{os.environ['AWS_REGION']}:{os.environ['USER_ID']}:inference-profile/us.amazon.nova-micro-v1:0"
             }
         }
-    }
-
-    command = bedrock_agent_runtime_client.RetrieveAndGenerateCommand(input_data)
-    response = bedrock_agent_runtime_client.send(command)
+    )
 
     return {
-        "response": response.output.text
+        "response": response
     }
+
