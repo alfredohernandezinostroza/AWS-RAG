d2ef2234563a415a182b20c1aab679e377d5ce17
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2025-07-02 15:48:11 +0100

    docs: add commented alternative for using the aws profile's default region

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 class EnvSettings:
     ACCOUNT_ID =  os.getenv('CDK_DEFAULT_ACCOUNT') 
     ACCOUNT_REGION = "us-east-2"
+    # ACCOUNT_REGION = os.getenv('CDK_DEFAULT_REGION') #We could use this if we wanted the default region
     RAG_PROJ_NAME = "kb-cdk"
