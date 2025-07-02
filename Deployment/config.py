6769841df389ef358afe0e0c5dac14d9644d4fa5
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2025-07-02 19:31:23 +0100

    feat: switch to us-eas-1

--- Deployment/config.py
+++ Deployment/config.py
@@ -64,7 +64,7 @@ EMBEDDING_MODEL_IDs = ["amazon.titan-embed-text-v1", "amazon.titan-embed-text-v2
 CHUNKING_STRATEGIES = {0:"Default chunking",1:"Fixed-size chunking", 2:"No chunking"}
 class EnvSettings:
     ACCOUNT_ID =  os.getenv('CDK_DEFAULT_ACCOUNT') 
-    ACCOUNT_REGION = "us-east-2"
+    ACCOUNT_REGION = "us-east-1"
     # ACCOUNT_REGION = os.getenv('CDK_DEFAULT_REGION') #We could use this if we wanted the default region
     RAG_PROJ_NAME = "kb-cdk"
 
